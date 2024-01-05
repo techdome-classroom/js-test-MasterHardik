cd8d2efc67075811838bd16380b0e7012ddb5a6a
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2024-01-05 13:19:18 +0530

    Assignment Solution Submission

--- program1.js
+++ program1.js
@@ -2,10 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+  let stk = [];
+  const bracketsPair = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  for (let char of s) {
+    if (char in bracketsPair) {
+      stk.push(char);
+    } else {
+      let last = stk.pop();
+      if (bracketsPair[last] !== char) return false;
+    }
+  }
+  return stk.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -2,9 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+var romanToInt = function (s) {
+  const sym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let ans = 0;
+  for (let i = 0; i < s.length; i++) {
+    let curr = sym[s[i]];
+    let next = sym[s[i + 1]];
+    curr < next ? (ans -= curr) : (ans += curr);
+  }
+  return ans;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
